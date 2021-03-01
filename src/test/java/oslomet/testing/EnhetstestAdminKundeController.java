1d4fcfb0253a96becd213a80c5effcd3a50ba5f2
jørgen <s331352@oslomet.no>
2021-03-01 21:00:51 +0100

    testing kunde

---
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -0,0 +1,9 @@
+package oslomet.testing;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetstestAdminKundeController {
+
+}
